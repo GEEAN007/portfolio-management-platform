267fc2585a9d738ba028cf19467e8ebdc5f56f6c
GEEAN007 <gyanaranjan6371@gmail.com>
2026-02-17 00:42:51 +0530

    Added HealthController API endpoint

---
+++ backend/src/main/java/com/gyana/portfolio/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.gyana.portfolio.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Portfolio Management Backend Running Successfully ";
+    }
+}
