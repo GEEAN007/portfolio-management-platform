11beae78acfdf63c582f68225360ab2665d26449
GEEAN007 <gyanaranjan6371@gmail.com>
2026-02-24 00:42:16 +0530

    Use environment API URL

--- frontend-client/src/services/api.js
+++ frontend-client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://portfolio-management-platform-3.onrender.com"
+  baseURL: "import.meta.env.VITE_API_URL"
 });
 
 API.interceptors.request.use((req) => {
